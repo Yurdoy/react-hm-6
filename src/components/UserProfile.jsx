9fe1a53d0a7bf4830db843c12f9a9050f43e0048
Yurdoy <yurdoy@gmail.com>
2025-05-28 14:58:23 +0200

    UserProfile.jsx component and UserProfile.module.css created. useState for user defined. UseEffect used for loading user data. fetchUser function created and successfully tested. The condition of checking loading of data creating and tested - all working fine.

---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import cls from "../components/UserProfile.module.css";
+
+const UserProfile = () => {
+  const [userStatus, setUserStatus] = useState(null);
+
+  const fetchUser = async () => {
+    try {
+      const response = await axios.get("https://randomuser.me/api");
+      setUserStatus(response.data.results[0]);
+      console.log("User data", response.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      console.log("Request completed");
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <div className={cls}>
+      {userStatus ? (
+        <div>
+          <img src={userStatus.picture.large} alt="User Profile" />
+          <h2>
+            {userStatus.name.first} {userStatus.name.last}
+          </h2>
+          <p>Email: {userStatus.email}</p>
+          <p>Phone: {userStatus.phone}</p>
+          <button onClick={fetchUser}>Load New User</button>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default UserProfile;
